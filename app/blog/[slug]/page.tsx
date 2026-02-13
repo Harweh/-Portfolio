1ab4c6a39518a6fc91b3815afd5ab84a1fc4ec0f
Emmy <e.awetobi@gmail.com>
2026-02-14 00:40:44 +0100

    fixed

--- app/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function BlogPost({params}: {params: {slug: string}}) {
-    return (
-        <div>
-            <h2>{params.slug}</h2>
-        </div>
-    )
-}
